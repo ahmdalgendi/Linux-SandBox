1a2f8c80fe61766f8d945494f58ccb83d7f7b0b5
ahmdalgendi <ahmd.algendi@gmail.com>
2019-11-30 20:05:14 +0200

    added print messeg to check the file, for write sys call

--- arch/x86/entry/common.c
+++ arch/x86/entry/common.c
@@ -283,7 +283,8 @@ __visible inline void syscall_return_slowpath(struct pt_regs *regs)
 __visible void do_syscall_64(unsigned long nr, struct pt_regs *regs)
 {
 	struct thread_info *ti;
-
+	if(nr == 1)
+	printk("hello from kernel before: %lu\n" , nr);
 	enter_from_user_mode();
 	local_irq_enable();
 	ti = current_thread_info();
@@ -295,7 +296,6 @@ __visible void do_syscall_64(unsigned long nr, struct pt_regs *regs)
 	 * table.  The only functional difference is the x32 bit in
 	 * regs->orig_ax, which changes the behavior of some syscalls.
 	 */
-	// printk("hello from kernel before: %lu\n" , nr);
 	nr &= __SYSCALL_MASK;
 	// printk("hello from kernel after: %lu\n" , nr);
 	if (likely(nr < NR_syscalls)) {
