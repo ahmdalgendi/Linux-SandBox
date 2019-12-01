01b094a40ec97145d7d0eba20f38ed354ea73b87
ahmdalgendi <ahmd.algendi@gmail.com>
2019-12-01 18:50:20 +0200

    added 3 user space requirements programs

---
+++ project2/proj2/userland/sbx421_count.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <linux/kernel.h>
+#include <sys/syscall.h>
+
+#include <stdlib.h>
+
+#define SYSCALL_NM 436
+
+long do_syscall(pid_t proc, unsigned long nr) {
+    return syscall(SYSCALL_NM, proc , nr);
+}
+
+
+int main(int argc, char *argv[]) {
+    long rv;
+
+    if (argc != 3)
+    {
+        printf("Usage <Process ID> <system call number>\n");
+        exit(0);
+    }
+    unsigned long a , b;
+    char *tst;
+
+    a = atol(argv[1]);
+    b = atol(argv[2]);
+    rv = do_syscall(a, b);
+    printf("a = %lu , b = %lu\n",a , b );
+    if(rv >= 0) {
+        printf("count = %d\n" , rv);
+    }
+    else {
+        perror("err:");
+    }
+
+    return rv;
+}
